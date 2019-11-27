92422392daf66d962a0166d27b1483620d2e60de
rnachaloor <rnachaloor@gmail.com>
2019-11-26 23:07:55 -0500

    Test

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client
+var x;
+const prefix = '!roth'
+
+client.once('ready', () => {
+    console.log('Hey');
+});
+
+
+client.on('message', message => {
+    var mention = message.mentions.users.first();
+
+    if (message.content.startsWith (prefix)) {
+        if (mention == null) { return; }
+        message.delete();
+        if (mention.username == 'Evan') {
+            mentionMessage = message.content.slice (5);
+            for (x = 0; x < 1; x++) {
+                mention.sendMessage ("IU SUCKS");
+                mention.sendMessage (mentionMessage);
+            }
+        }
+        mentionMessage = message.content.slice (5);
+        for (x = 0; x < 1; x++) {
+            mention.sendMessage (mentionMessage);
+        }
+        message.channel.send("Just sent it");
+    }
+
+    /* if (message.content === prefix){
+        for (x = 0; x < 5; x++) {
+            message.channel.send('<@326427639294459905>');
+        }
+    }*/
+})
+
+client.login(process.env.BOT_TOKEN);
