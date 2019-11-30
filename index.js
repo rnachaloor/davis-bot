fbd805c4a2c8ac0a4f2e8726dec3e1f1db4b46f8
rnachaloor <rnachaloor@gmail.com>
2019-11-29 20:00:48 -0500

    Reverted More

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+var x;
 const prefix = '!roth'
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${index.js}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-}
 
 client.once('ready', () => {
     console.log('Hey');
@@ -19,27 +9,24 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
-    
-    //client.commands.get('mentionMessage').execute(message, args);
-
     var mention = message.mentions.users.first();
 
-        if (message.content.startsWith (prefix)) {
-            if (mention == null) { return; }
-            message.delete();
-            if (mention.username == 'Evan') {
-                mentionMessage = message.content.slice (5);
-                for (x = 0; x < 1; x++) {
-                    mention.sendMessage ("IU SUCKS");
-                    mention.sendMessage (mentionMessage);
-                }
-            }
+    if (message.content.startsWith (prefix)) {
+        if (mention == null) { return; }
+        message.delete();
+        if (mention.username == 'Evan') {
             mentionMessage = message.content.slice (5);
-            for (x = 0; x < 5; x++) {
+            for (x = 0; x < 1; x++) {
+                mention.sendMessage ("IU SUCKS");
                 mention.sendMessage (mentionMessage);
             }
-            message.channel.send("Just sent it");
         }
+        mentionMessage = message.content.slice (5);
+        for (x = 0; x < 5; x++) {
+            mention.sendMessage (mentionMessage);
+        }
+        message.channel.send("Just sent it");
+    }
 
     if (message.content === prefix){
         for (x = 0; x < 5; x++) {
