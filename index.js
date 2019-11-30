e4446ebfd825079bbcb498ec324f8c105debceb6
rnachaloor <rnachaloor@gmail.com>
2019-11-29 19:57:57 -0500

    Reverted changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 const prefix = '!roth'
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./commands/${index.js}`);
 
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
@@ -20,7 +20,26 @@ client.once('ready', () => {
 
 client.on('message', message => {
     
-    client.commands.get('mentionMessage').execute(message, args);
+    //client.commands.get('mentionMessage').execute(message, args);
+
+    var mention = message.mentions.users.first();
+
+        if (message.content.startsWith (prefix)) {
+            if (mention == null) { return; }
+            message.delete();
+            if (mention.username == 'Evan') {
+                mentionMessage = message.content.slice (5);
+                for (x = 0; x < 1; x++) {
+                    mention.sendMessage ("IU SUCKS");
+                    mention.sendMessage (mentionMessage);
+                }
+            }
+            mentionMessage = message.content.slice (5);
+            for (x = 0; x < 5; x++) {
+                mention.sendMessage (mentionMessage);
+            }
+            message.channel.send("Just sent it");
+        }
 
     if (message.content === prefix){
         for (x = 0; x < 5; x++) {
